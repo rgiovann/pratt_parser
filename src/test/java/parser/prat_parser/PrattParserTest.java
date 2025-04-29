e5b37caf1173d1eae764fc0937605e22934dfdc1
Giovani L. Rozza <giovanni.rozza@gmail.com>
2025-04-29 20:54:38 -0300

    Refatorado testes do parser, agora compara a estrutura AST e não o resultado toString.

--- src/test/java/parser/prat_parser/PrattParserTest.java
+++ src/test/java/parser/prat_parser/PrattParserTest.java
@@ -1,52 +1,177 @@
 package parser.prat_parser;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
 import parser.prat_parser.lexer.LexerFactory;
+import parser.prat_parser.model.Atom;
+import parser.prat_parser.model.Cons;
+import parser.prat_parser.model.Expression;
 
 public class PrattParserTest {
 
-    private void assertParsed(String input, String expected) {
-        var lexer = LexerFactory.createDefaultLexer(input);
-        var parser = new PrattParser(lexer);
-        var expression = parser.parse();
-        assertEquals(expected, expression.toString());
+    private Expression parse(String input) {
+        PrattParser parser = new PrattParser(LexerFactory.createDefaultLexer(input));
+        return parser.parse();
     }
 
-    @Test
-    public void testSingleNumber() {
-        assertParsed("1", "1");
+    private static Atom a(String value) {
+        return new Atom(value);
     }
 
-    @Test
-    public void testSimpleAddition() {
-        assertParsed("1 + 2", "(+ 1 2)");
+    private static Cons c(String op, Expression... operands) {
+        return new Cons(op, List.of(operands));
     }
 
-    @Test
-    public void testPrecedence() {
-        assertParsed("1 + 2 * 3", "(+ 1 (* 2 3))");
+    @Nested
+    class ArithmeticTests {
+
+        @Test
+        void testSingleNumber() {
+            assertEquals(a("1"), parse("1"));
+        }
+
+        @Test
+        void testSimpleAddition() {
+            assertEquals(c("+", a("1"), a("2")), parse("1 + 2"));
+        }
+
+        @Test
+        void testAdditionAndMultiplication() {
+            assertEquals(c("+", a("1"), c("*", a("2"), a("3"))), parse("1 + 2 * 3"));
+        }
+
+        @Test
+        void testComplexExpression() {
+            assertEquals(
+                c("+",
+                    c("+", a("a"), c("*", c("*", a("b"), a("c")), a("d"))),
+                    a("e")
+                ),
+                parse("a + b * c * d + e")
+            );
+        }
+
+        @Test
+        void testParentheses() {
+            assertEquals(
+                c("*", c("+", a("1"), a("2")), a("3")),
+                parse("(1 + 2) * 3")
+            );
+        }
+
+        @Test
+        void testParenthesizedExpression() {
+            assertEquals(a("0"), parse("(((0)))"));
+        }
     }
 
-    @Test
-    public void testParentheses() {
-        assertParsed("(1 + 2) * 3", "(* (+ 1 2) 3)");
+    @Nested
+    class OperatorTests {
+
+        @Test
+        void testFunctionComposition() {
+            assertEquals(
+                c(".", a("f"), c(".", a("g"), a("h"))),
+                parse("f . g . h")
+            );
+        }
+
+        @Test
+        void testMixedExpression() {
+            assertEquals(
+                c("+",
+                    c("+", a("1"), a("2")),
+                    c("*",
+                        c("*", c(".", a("f"), c(".", a("g"), a("h"))), a("3")),
+                        a("4")
+                    )
+                ),
+                parse("1 + 2 + f . g . h * 3 * 4")
+            );
+        }
+
+        @Test
+        void testPrefixMinus() {
+            assertEquals(
+                c("*",
+                    c("-", c("-", a("1"))),
+                    a("2")
+                ),
+                parse("--1 * 2")
+            );
+        }
+
+        @Test
+        void testPrefixMinusWithComposition() {
+            assertEquals(
+                c("-", c("-", c(".", a("f"), a("g")))),
+                parse("--f . g")
+            );
+        }
+
+        @Test
+        void testPostfixFactorial() {
+            assertEquals(
+                c("-", c("!", a("9"))),
+                parse("-9!")
+            );
+        }
+
+        @Test
+        void testCompositionWithFactorial() {
+            assertEquals(
+                c("!", c(".", a("f"), a("g"))),
+                parse("f . g !")
+            );
+        }
+
+        @Test
+        void testDotAndPostfix() {
+            assertEquals(
+                c("!", c(".", a("f"), a("g"))),
+                parse("f . g !")
+            );
+        }
     }
 
-    @Test
-    public void testUnary() {
-        assertParsed("-9!", "(- (! 9))");
-    }
+    @Nested
+    class AdvancedTests {
 
-    @Test
-    public void testDotAndPostfix() {
-        assertParsed("f . g !", "(! (. f g))");
-    }
+        @Test
+        void testArrayIndexingStructure() {
+            assertEquals(
+                c("[",
+                    c("[", a("x"), a("0")),
+                    a("1")
+                ),
+                parse("x[0][1]")
+            );
+        }
 
-    @Test
-    public void testNestedTernary() {
-        assertParsed("a ? b : c ? d : e", "(? a b (? c d e))");
+        @Test
+        void testTernaryOperator() {
+            assertEquals(
+                c("?",
+                    a("a"),
+                    a("b"),
+                    c("?", a("c"), a("d"), a("e"))
+                ),
+                parse("a ? b : c ? d : e")
+            );
+        }
+
+        @Test
+        void testAssignmentAndTernary() {
+            assertEquals(
+                c("=",
+                    a("a"),
+                    c("=", c("?", a("0"), a("b"), a("c")), a("d"))
+                ),
+                parse("a = 0 ? b : c = d")
+            );
+        }
     }
 }
