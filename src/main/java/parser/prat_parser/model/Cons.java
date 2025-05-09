86f841247c7d4fb1033fc1c511b96f6262e811a4
Giovanni <giovanni.rozza@gmail.com>
2025-05-09 09:32:56 -0300

    Update Cons.java
    teste concluido - removido blocos de comentário dummy

--- src/main/java/parser/prat_parser/model/Cons.java
+++ src/main/java/parser/prat_parser/model/Cons.java
@@ -5,14 +5,6 @@ import java.util.stream.Collectors;
 
 public record Cons(String operator, List<Expression> operands) implements Expression {
 
-    /*
-    bloco de codigo 1
-
-
-    
-    */
-
-
     @Override
     public String toString() {
         if (operands.isEmpty()) {
@@ -24,15 +16,5 @@ public record Cons(String operator, List<Expression> operands) implements Expres
                 .collect(Collectors.joining(" ")) +
         ")";
     }
-
-    /*
-    bloco de codigo 2
-    eww
-    we we
-    ewwe we werweewr we rwer ew
-     we werwer wweer wer wer
-
-    
-    */
     
 }
