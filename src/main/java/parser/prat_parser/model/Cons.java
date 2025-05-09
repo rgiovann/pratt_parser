af64fca771f9908f150b257371e421ec29a027c6
Giovanni <giovanni.rozza@gmail.com>
2025-05-09 09:26:37 -0300

    Update Cons.java
    teste para o extrator de arquivos .java (outro projeto), inclui esses blocos de codigo dummy para verificar se extrator vai extrair esse java e remover os blocos de codigo.

--- src/main/java/parser/prat_parser/model/Cons.java
+++ src/main/java/parser/prat_parser/model/Cons.java
@@ -4,6 +4,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public record Cons(String operator, List<Expression> operands) implements Expression {
+
+    /*
+    bloco de codigo 1
+
+
+    
+    */
+
+
     @Override
     public String toString() {
         if (operands.isEmpty()) {
@@ -15,5 +24,15 @@ public record Cons(String operator, List<Expression> operands) implements Expres
                 .collect(Collectors.joining(" ")) +
         ")";
     }
+
+    /*
+    bloco de codigo 2
+    eww
+    we we
+    ewwe we werweewr we rwer ew
+     we werwer wweer wer wer
+
+    
+    */
     
 }
